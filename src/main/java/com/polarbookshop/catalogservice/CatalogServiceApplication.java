854c95d4037c204f18debef77b28f1e33dc5abe3
Piter80 <petrzl@bk.ru>
2024-06-28 12:02:04 +0300

    add jdbc

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CatalogServiceApplication.class, args);
+		/*PolarProperties polarProperties = run.getBean(PolarProperties.class);
+		System.out.println(polarProperties.getGreeting());*/
+
 	}
 
 }
